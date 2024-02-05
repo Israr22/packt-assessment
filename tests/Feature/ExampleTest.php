780561611cca5d76c83e62315c32749f32f94dca
Mohd Israr <israrraza942001@gmail.com>
2024-02-05 23:31:49 +0530

    Update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Book;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -15,5 +17,46 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_the_application_returns_it_returns_all_books()
+    {
+        // Arrange
+        // Book::factory()->count(3)->create();
+
+        // Act
+        $response = $this->get('/api/book');
+
+        // Assert
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data');
+    }
+
+    public function test_the_application_returns_it_returns_a_specific_book()
+    {
+        // Arrange
+        $book = Book::factory()->create();
+
+        // Act
+        $response = $this->get("/api/book/{$book->id}");
+
+        // Assert
+        $response->assertStatus(200)
+            ->assertJson($book->toArray());
+    }
+
+    public function  test_the_application_returns_it_creates_a_new_book()
+    {
+        // Arrange
+        $bookData = Book::factory()->make()->toArray();
+
+        // Act
+        $response = $this->post('/api/book', $bookData);
+
+        // Assert
+        $response->assertStatus(201)
+            ->assertJson(['message'=>'Book Created Successfully!!','book' => $bookData]);
+
+        $this->assertDatabaseHas('books', $bookData);
     }
 }
